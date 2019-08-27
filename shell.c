0185f064ee8da7ba5b05ad54ed78e7e5a7181a56
apatinoc4 <apatinoc4@gmail.com>
2019-08-27 20:30:26 +0000

    updated shell file

--- shell.c
+++ shell.c
@@ -2,26 +2,52 @@
 /**
 * main - main function of our shell
 *
+* @argc: number of things in the command line (unused)
+*
+* @argv: array of string values
+*
 * Return: 0 on success
 */
-int main(void)
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	char *line;
-	char **args;
+	char *line = NULL;
+	char **args = NULL;
 	char *str = "#cisfun$ ";
 	int status;
+	int loop_count = 0;
+	pid_t _loop;
+
+	signal(SIGINT, SIG_IGN);
 
 	do {
-		_puts(str);
-
+		if (isatty(STDIN_FILENO))
+		{
+			_puts(str);
+		}
 		line = read_line();
+
+		if (line == NULL)
+		{
+			/**free(line);*/
+			errno = EINVAL, perror(argv[0]);
+			return (-1);
+		}
 		args = parse_line(line);
-		status = execute(args);
+
+		if (_strcmp(args[0], "exit") == 0)
+		{
+			status = args[1] == NULL ? 0 : _atoi(args[1]);
+			free(line), free(args);
+			return (status);
+		}
+		_loop = run(argv[0], line, args, loop_count);
 
 		free(line);
 		free(args);
 
-	} while (status);
+		loop_count++;
+
+	} while (_loop > 0);
 
 	return (0);
 }
