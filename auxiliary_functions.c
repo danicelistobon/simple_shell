8f712c2af9f6b749a62d118c8e258165d73dd566
danicelistobon <danielct-97@hotmail.com>
2019-08-27 20:17:40 +0000

    created strfind function

--- auxiliary_functions.c
+++ auxiliary_functions.c
@@ -21,3 +21,29 @@ int _strlen(char *s)
 	}
 	return (len);
 }
+
+/**
+* _strfind - find a character in a string
+*
+* @c: character to find
+*
+* @s: string
+*
+* Return: 1 if found or 0 on otherwise
+*/
+int _strfind(char c, char *s)
+{
+	if (s == NULL)
+	{
+		return (0);
+	}
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (1);
+		}
+		s++;
+	}
+	return (0);
+}
