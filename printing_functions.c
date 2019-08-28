43855aeb6d66e0ccecb53d61795f62e30c6d5111
apatinoc4 <apatinoc4@gmail.com>
2019-08-28 03:51:00 +0000

    updated printing functions

--- printing_functions.c
+++ printing_functions.c
@@ -27,3 +27,81 @@ int _putchar(char c)
 {
 	return (write(STDOUT_FILENO, &c, 1));
 }
+
+/**
+* print_error - prints error messages
+*
+* @prog: program name
+*
+* @loop_count: counter
+*
+* @line: user input given by read function
+*
+* Return: no return
+*/
+void print_error(char *prog, int loop_count, char *line)
+{
+	int value;
+	char buffer[20];
+
+	value = loop_count + 1;
+
+	snprintf(buffer, 10, "%d", value);
+
+	write(STDERR_FILENO, prog, _strlen(prog));
+
+	write(STDERR_FILENO, ": ", 2);
+
+	write(STDERR_FILENO, buffer, _strlen(buffer));
+
+	write(STDERR_FILENO, ": ", 2);
+
+	write(STDERR_FILENO, line, _strlen(line));
+
+	write(STDERR_FILENO, ": ", 2);
+
+	if (errno == EACCES)
+	{
+		write(STDERR_FILENO, "Permission denied\n", 18);
+	}
+	else
+	{
+		write(STDERR_FILENO, "not found\n", 10);
+	}
+}
+
+/**
+* _atoi - convert a string to a number
+*
+* @s: string to convert
+*
+* Return: number or 0 if string can't be converted
+*/
+int _atoi(char *s)
+{
+	int found = 0;
+	unsigned int num = 0;
+	int n = 1;
+
+	while (*s != '\0')
+	{
+		if (*s == '-')
+		{
+			n *= -1;
+		}
+		else if (*s >= '0' && *s <= '9')
+		{
+			if (!found)
+			{
+				found = 1;
+			}
+			num = num * 10 + (*s - '0');
+		}
+		else
+		{
+			return (0);
+		}
+		s++;
+	}
+	return (num * n);
+}
